7c5da2b9de998fcd89e63026e87527e4598b69f3
Charlotte Mountain <charr.mt@gmail.com>
2020-03-17 23:36:36 -0700

    works

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
-# Code-Quiz
-Coding Quiz
+#  **Password Generator** #
+
+## **Description**
+
+-  User can take a quiz to test their knowledge with 5 simple questions about software development, and syntax in coding. By pressing "Start Quiz" they start the quiz! Then begins the 75 second timer, and each time the user gets a question wrong they get -10 seconds from the clock. Every time they answer correctly the user gets +10 added to their score. At the end they are taken to the high scores page where they may input their scores, and see their name on the scoreboard! They may also reset the scoreboard with the "Reset" button. Then they may return home by clicking the "Go Back" button. 
+
+    - ### **HTML pages:**
+         1. [home.html](index.html) is the main home page for the quiz
+         2. [home-script.js](portfolio.html) is the JavaScript for script to make quiz run correctly.
+         3. [highscores.html](highscores.html)
+         4. [highscores-script.js](highscores-script.js)
 
 
-- AS A coding bootcamp student.
-- I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-- SO THAT I can gauge my progress compared to my peers
+    - ### **Style Sheets** (used on all HTML pages)**:**
+         1. [style.css](style.css) (created by Coding)
+         2. [Bootstrap] (https://getbootstrap.com/)
+
+
+## **Visuals**
+- Prompted with 5 questions
+- High Scores page will display at the end
+
+## **Usage**
+- Quiz for new coding developers
+
+## **Support**
+- e-mail: charr.mt@gmail.com
+- https://github.com/charrmountain/coding-quiz
--- highscore-script.js
+++ highscore-script.js
@@ -1,7 +1,8 @@
-var initialInput = document.getElementById("initialInput");
+var formEl = document.querySelector("form");
+var itemInput = document.querySelector("#item-input");
 var userHighscore = document.getElementById("userScore");
-var submitBtn = document.querySelector(".btn btn-primary");
-var initialDisplay=document.getElementById("initialDisplay");
+var resetBtn = document.getElementById("reset");
+
 
 // get the list from storage
 var userScore = JSON.parse(localStorage.getItem("newScore"));
@@ -9,30 +10,28 @@ var userScore = JSON.parse(localStorage.getItem("newScore"));
 //take sore and put initials
 userHighscore.textContent="Your final score is " + userScore ;
 
-var userInitials = [];
-
-//when user clicks submit
-submitBtn.addEventListener("click",function(event) {
+    var userInitials = [];
+  
+  formEl.addEventListener("submit", function(event) {
     event.preventDefault();
+    // get input
+    var newItem = itemInput.value.trim();
+   
+    // add input to list
+    userInitials.push(newItem)
+  
+    // add to local storage
+    localStorage.setItem("userInitials", JSON.stringify(userInitials))
     
-    userInitials.push(initialInput)
-
-    localStorage.setItem("initials", JSON.stringify(userInitials))
-
-    // // get user from local storage
-    // var list = JSON.parse(localStorage.getItem("initials", "newScore"))
-    // var listElement = document.getElementById("initialDisplay");
-
-    // // display the list
-    // for (var i = 0; i < list.length; i++) {
-    // var newLiElement = document.createElement("li");
-    // newLiElement.textContent = list[i];
-    // listElement.appendChild(newLiElement);
-    // }
-
-    // if (list.length === 0) {
-    // var messageElement = document.createElement("h2");
-    // messageElement.textContent = "No items yet!"
-    // document.body.appendChild(messageElement)
-    // }
+    var userInitialarr= JSON.parse(localStorage.getItem("userInitials"));
+    console.log(userInitialarr)
+    var newLiElement = document.createElement("li");
+    newLiElement.textContent = userInitialarr + " " + userScore;
+    userHighscore.appendChild(newLiElement);
+    
 });
+
+//reset Button
+resetBtn.addEventListener('click', function () {
+    userHighscore.value = '';
+}, false);
